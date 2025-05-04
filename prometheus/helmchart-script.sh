3499d7d0f4eda70241afc2676e82cf3a03c68c78
Nathan Li <nathan.li.1995@gmail.com>
2025-05-04 19:40:26 -0400

    change to use manual deployment files

--- prometheus/helmchart-script.sh
+++
@@ -1,6 +0,0 @@
-# https://github.com/prometheus-community/helm-charts
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm install prometheus prometheus-community/prometheus --namespace prometheus
-
-# if modified the prometheus-values.yml file, run the following command to update the helm chart
-helm upgrade prometheus prometheus-community/prometheus --namespace prometheus --values prometheus/prometheus-values.yml
