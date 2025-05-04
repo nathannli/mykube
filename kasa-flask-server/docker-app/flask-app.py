05980a570211451aebf46c176161d5ae732acb5d
Nathan Li <nathan.li.1995@gmail.com>
2025-05-04 16:31:13 -0400

    update flask app

--- kasa-flask-server/docker-app/flask-app.py
+++ kasa-flask-server/docker-app/flask-app.py
@@ -1,44 +1,44 @@
 from flask import Flask, Response
-from kasa import SmartPlug
-from prometheus_client import CollectorRegistry, Gauge, generate_latest
+from prometheus_client import Summary
 import asyncio
 import os
+import asyncio
+from kasa import Device, Module
 
 HS300_IP = os.environ.get("PLUG_IP")
 
 app = Flask(__name__)
 
 async def get_metrics():
-    plug = SmartPlug(HS300_IP)
-    await plug.update()
-    emeter = plug.emeter_realtime if plug.has_emeter else {}
+    output_dict = {}
 
-    return {
-        "power_watts": emeter.get("power_mw", 0) / 1000,
-        "voltage_volts": emeter.get("voltage_mv", 0) / 1000,
-        "current_amps": emeter.get("current_ma", 0) / 1000,
-        "energy_kwh": emeter.get("total_wh", 0) / 1000,
-    }
+    dev = await Device.connect(host=HS300_IP)
+    await dev.update()
+    str = ""
+    str += f"Host: {dev.host}\n"
+    str += f"Alias: {dev.alias}\n"
+    str += f"Model: {dev.model}\n"
+    str += f"Device Type: {dev.device_type}\n"
+    for plug in dev.children:
+        str += f"Plug: {plug.alias}\n"
+        energy = plug.modules[Module.Energy]
+        str += f"Current Consumption: {energy.current_consumption}w\n"
+        output_dict[plug.alias] = energy.current_consumption
+    return str
 
 @app.route("/metrics")
 def metrics():
-    registry = CollectorRegistry()
-    power = Gauge("kasa_power_watts", "Power consumption in watts", registry=registry)
-    voltage = Gauge("kasa_voltage_volts", "Voltage in volts", registry=registry)
-    current = Gauge("kasa_current_amps", "Current in amps", registry=registry)
-    energy = Gauge("kasa_energy_kwh", "Total energy used in kWh", registry=registry)
+    # s = Summary("kasa_power_watts", "Power consumption in watts")
 
     # Run asyncio task inside Flask
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     data = loop.run_until_complete(get_metrics())
+    return data
+    # for key, value in data.items():
 
-    power.set(data["power_watts"])
-    voltage.set(data["voltage_volts"])
-    current.set(data["current_amps"])
-    energy.set(data["energy_kwh"])
 
-    return Response(generate_latest(registry), mimetype="text/plain")
+    # return Response(generate_latest(registry), mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9100)
