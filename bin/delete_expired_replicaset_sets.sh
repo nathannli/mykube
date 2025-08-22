54e08d00ec290ca7d5d72b511dcacffd59505d58
Nathan Li <nathan.li.1995@gmail.com>
2025-08-21 20:02:23 -0400

    add utility script

---
+++ bin/delete_expired_replicaset_sets.sh
@@ -0,0 +1,3 @@
+kubectl get rs -n kasa-flask-server \
+            -o jsonpath='{range .items[?(@.status.replicas==0)]}{.metadata.name}{"\n"}{end}' \
+          | xargs -r kubectl delete rs -n kasa-flask-server
