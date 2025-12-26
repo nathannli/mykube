59d0524d4e0122f7ae192db567f1925e44809f1f
Nathan Li <nathan.li.1995@gmail.com>
2025-12-26 03:45:17 -0500

    add missing helm line

--- kubernetes-dashboard/deploy.sh
+++ kubernetes-dashboard/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # create namespace if not exist
 kubectl get namespace kubernetes-dashboard || kubectl create namespace kubernetes-dashboard
-
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm install kubernetes-dashboard/kubernetes-dashboard --namespace kubernetes-dashboard --generate-name
 
 kubectl apply -f ./
