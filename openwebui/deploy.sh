a4d7fa4c1998f11c87d8dad9d8e7c1d8c1e2775e
Nathan Li <nathan.li.1995@gmail.com>
2026-04-13 22:10:17 -0400

    feat(openwebui): add deploy script

---
+++ openwebui/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# create namespace if not exist
+kubectl get namespace openwebui >/dev/null 2>&1 || kubectl create namespace openwebui
+
+# deploy openwebui resources
+kubectl apply -f ./
